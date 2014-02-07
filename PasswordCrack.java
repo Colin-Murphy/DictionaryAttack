91fda3626a8dede86366dc6e3be1e3e8cc9d533d
Colin Murphy <colinmurphy1993@gmail.com>
2014-02-07 13:44:01 -0500

    Added basic io for dictionaryFile and added a User class to model the user/password relationship

--- PasswordCrack.java
+++ PasswordCrack.java
@@ -3,7 +3,7 @@
 	provided dictionary file. It imlements multi threading and is an example
 	of proper thread design.
 	
-	Designed for RIT Concepts of Paralel and Distributed Systems
+	Designed for RIT Concepts of Paralel and Distributed Systems Project 1
 	
 	@author Colin L Murphy <clm3888@rit.edu>
 	@version 2/7/14
@@ -17,6 +17,16 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 //Read files
 import java.io.File;
+//Arraylist for storing users and hashed passwords
+import java.util.ArrayList;
+//Buffered reader
+import java.io.BufferedReader;
+//File reader
+import java.io.FileReader;
+//Io exception
+import java.io.IOException;
+//File not found
+import java.io.FileNotFoundException;
 
 
 public class PasswordCrack {
@@ -46,12 +56,8 @@ public class PasswordCrack {
 			return;
 		}
 		
-	
-		
-		
-		
-		
-		new PasswordCrack();
+		//If we got here then the input appears to be good
+		new PasswordCrack(dictionaryFile, databaseFile);
 				
 	}//End of main
 	
@@ -62,12 +68,42 @@ public class PasswordCrack {
 		
 		Implements multithreaded techniques and design patterns
 		
-		@param String dictionaryFile the path to the dictionary file
-		@param String databaseFile the path to the database file
+		@param File dictionaryFile the the dictionary file
+		@param String databaseFile the the database file
 	*/
-	public PasswordCrack() {
-		String hash = sha256("12345");
-		System.out.println(hash);
+	public PasswordCrack(File dictionaryFile, File databaseFile) {
+		
+		ArrayList<String> passwords = new ArrayList<String>();
+		ArrayList<User> users = new ArrayList<User>();
+		
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader
+				(new FileReader(dictionaryFile));
+		}
+		
+		catch(FileNotFoundException e) {
+			System.err.println("File " + dictionaryFile.getName() + 
+				" does not exist");
+			return;
+		}
+			
+		String line = "";
+		try {
+			while ((line = reader.readLine()) !=null) {
+					passwords.add(line.trim());
+			}
+		}
+		
+		catch(IOException e) {
+			System.err.println("Error reading " + dictionaryFile.getName());
+			return;
+		}
+		
+		for (String password:passwords) {
+			System.out.println(password);
+		}
+
 	}//End of PasswordCrack
 	
 	/**
---
+++ User.java
@@ -0,0 +1,41 @@
+/**
+	Models a user from the database file to make storing and accessing easier.
+	
+	Designed for RIT Concepts of Paralel and Distributed Systems Project 1
+	
+	@author Colin L Murphy <clm3888@rit.edu>
+	@version 2/7/14
+*/
+
+public class User {
+	//Username for the user… wow this comment is useless
+	String username = "";
+	//Users password as represented by the SHA256 hash
+	String hashedPassword = "";
+	//Users password determined by the dictionary attack
+	String textPassword = "";
+	
+	public User(String username, String hashedPassword) {
+		this.username = username;
+		this.hashedPassword = hashedPassword;
+	}
+	
+	/**
+		Set the plain text password of a user.
+		@param String the plain text password to store
+	*/
+	public void setTextPassword(String textPassword) {
+		this.textPassword = textPassword;
+	}
+	
+	/**
+		Getter for the hashed password
+		@return String SHA256 representation of the users password
+	*/
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+	
+}
+	
+	
